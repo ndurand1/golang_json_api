652547354168ac71a780fc358bc3e61e9aa2cb8e
Nicholas Durand <nicholasdurand@NPD-Personal-Computing-Power-Device.local>
2025-06-17 20:57:17 +1000

    Web Server Complete with routing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module golang_json_api
+
+go 1.24.4
+
+require github.com/gorilla/mux v1.8.1
--- webServer.go
+++ webServer.go
@@ -9,14 +9,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// html.EscapeString = security measure to escape special HTML characters to avoid XSS
-func main() {
-
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+// root, welcome page
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+// endpoint 1
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+// endpoint 2
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	// clean user input with Escape String as this is rendering to HTML, prevents XSS
+	fmt.Fprintf(w, "Todo show: %s", html.EscapeString(vars["todoId"]))
+}
+
+func main() {
+	// StrictSlash redirects /users/ to /users, for example
+	router := mux.NewRouter().StrictSlash(true)
+	// handle calls to multiple endpoints
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoID}", TodoShow)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
