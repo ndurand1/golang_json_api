eb967750f4c90f790af5bec80138cfa53bcb7e30
Nicholas Durand <nicholasdurand@NPD-Personal-Computing-Power-Device.local>
2025-06-22 14:33:27 +1000

    adding JSON flag to header, adding status code

--- handlers/todo.go
+++ handlers/todo.go
@@ -23,6 +23,8 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		models.Todo{Name: "Ship laptop"},
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
